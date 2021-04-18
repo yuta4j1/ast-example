4ea8eadd52908e6a97f0256138c3c896d8f90636
yuta4j1 <kascado.ys10@gmail.com>
2021-04-18 15:45:11 +0900

    generatorを使ってコード変換してみる

--- index.ts
+++ index.ts
@@ -1,8 +1,26 @@
 import { parse } from "@babel/parser";
+import traverse from "@babel/traverse";
+import generate from "@babel/generator";
 
 // AST に変換
 const ast = parse(`
-const a = 1
+const a = 1;
+const b = 5;
+function sum(aNum, bNum) {
+  return aNum + bNum;
+}
+
+console.log(sum(a, b));
+
 `);
 
 console.log(JSON.stringify(ast, null, 2));
+
+traverse(ast, {
+  VariableDeclaration(path) {
+    path.node.kind = "var";
+  },
+});
+
+console.log("***** output *****")
+console.log(generate(ast).code);
